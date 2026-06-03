ab215653f604dc7fdaff2dd423ebb607008a6452
gabrielsvieira01 <89791905+gabrielsvieira01@users.noreply.github.com>
2026-06-03 17:34:12 -0300

    Atualizar o users.js

--- assets/js/users.js
+++ assets/js/users.js
@@ -15,6 +15,7 @@
  * ========================================================================== */
 
 window.USERS = {
+      "pedro": "senha",
   "gabriel": "gabriel978",
   "admin":  "integradora2025",
   "aluno":  "medicina",
