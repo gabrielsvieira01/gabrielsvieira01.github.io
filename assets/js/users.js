8cac760b843314882193d238e470c1cb474940d5
gabrielsvieira01 <89791905+gabrielsvieira01@users.noreply.github.com>
2026-06-04 10:55:44 -0300

    Add files via upload

--- assets/js/users.js
+++ assets/js/users.js
@@ -15,7 +15,67 @@
  * ========================================================================== */
 
 window.USERS = {
-  "gabriel":  "gabriel978",
-  "guilherme":  "gui01",
-  "turma":  "setimo7"
+  // --- Acessos gerais ---
+  "gabriel":      "gabriel978",
+  "guilherme":    "gui01",
+
+  // --- Alunos (gerado a partir de logins_turma.xlsx) ---
+  "joaoafonso":   "afonso429",      // João Afonso
+  "paulo":        "paulo681",       // Paulo Emanuel
+  "dudaleao":     "duda151",        // Maria Eduarda Leão
+  "leiliane":     "leiliane749",    // Leiliane
+  "amanda":       "amanda190",      // Amanda
+  "eisler":       "eisler915",      // Eisler
+  "marlon":       "marlon392",      // Marlon
+  "fernando":     "fernando267",    // Fernando
+  "isabella":     "bella368",       // Isabella Catarucci
+  "joaovitor":    "joaovitor361",   // João Vitor
+  "cristiane":    "cris821",        // Cristiane Gordiano
+  "henrique":     "henrique461",    // Henrique Vasconcelos
+  "cecilia":      "ceci960",        // Cecília Brandão
+  "dudadavid":    "duda2935",       // Maria Eduarda David
+  "gabriella":    "gabi710",        // Gabriella Raiany
+  "victor":       "victor905",      // Victor
+  "zaine":        "zaine326",       // Zaine
+  "leticia":      "leti460",        // Leticia Lopes
+  "rodrigo":      "rodrigo160",     // Rodrigo Lopes
+  "jussara":      "jussara629",     // Jussara
+  "anab":         "bia855",         // Ana Betariz
+  "anacarolina":  "carol708",       // Ana Carolina
+  "geise":        "geise879",       // Geise
+  "angelo":       "angelo949",      // Ângelo
+  "matheus":      "matheus239",     // Matheus Araujo
+  "sabrina":      "sabrina960",     // Sabrina
+  "analuisa":     "analuisa333",    // Ana Luisa
+  "mariajulia":   "maju231",        // Maria Júlia
+  "mell":         "mell648",        // Mell Gabriely
+  "nicolle":      "nicolle428",     // Nicolle
+  "joaopedro":    "joaopedro634",   // João Pedro Donato
+  "hebert":       "hebert186",      // Hebert Augusto
+  "pedrolucas":   "pedrolucas131",  // Pedro Lucas
+  "jessyca":      "jessyca556",     // Jessyca
+  "giovanna":     "giovanna616",    // Giovanna Lélis
+  "leonardo":     "leo164",         // Leonardo
+  "juliasami":    "juliasami532",   // Júlia Sâmi
+  "rafael":       "rafael297",      // Rafael Santana
+  "luizh":        "luizh177",       // Luiz Henrique
+  "viviane":      "viviane558",     // Viviane Cruz
+  "anaclara":     "anaclara578",    // Ana Clara
+  "luizg":        "luizg178",       // Luiz Gustavo
+  "emanuele":     "manu351",        // Emanuele
+  "joaomarcos":   "jmarcos147",     // João Marcos
+  "pedroh":       "pedroh503",      // Pedro Henrique Porto
+  "juliacruz":    "juliacruz631",   // Julia Cruz
+  "giovana":      "giovana386",     // Giovana Souza
+  "rennan":       "rennan999",      // Rennan Leles
+  "jose":         "jose598",        // José Guilherme
+  "duda":         "duda3329",       // Maria Eduarda Teixeira
+  "jessycam":     "jessycam536",    // Jessyca Maria
+  "ingrid":       "ingrid596",      // Ingrid Layla
+  "patricia":     "patricia979",    // Patricia Cristina
+  "luiza":        "luiza492",       // Luiza Alberta
+  "kaik":         "kaik423",        // Kaik
+  "gilberto":     "gilberto842",    // Gilberto
+  "paloma":       "paloma276",      // Paloma
+  "flavia":       "flavia598"       // Flavia
 };
